a468cf13839bf5ec5e733b27d11aca633d81d59f
Andrea Bellome <andrea.bellome@gmail.com>
2023-06-27 17:44:07 +0200

    admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\DashboardController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,14 +21,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/', [DashboardController::class, 'index'] )->name('dashboard');
+
 });
 
 require __DIR__.'/auth.php';
